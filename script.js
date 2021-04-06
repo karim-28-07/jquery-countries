ee029b08160cdcb2a1028f06d5ff7179f429d121
karim <karim.mezouar@gmail.com>
2021-04-06 15:45:39 +0200

    show the countries

--- script.js
+++ script.js
@@ -12,9 +12,23 @@ function getCountries() {
                 url: 'http://localhost:8000/countries',
                 success: function (data, status, response) {
 
+                    console.log(data[0]);
+
                     
 
-                    $("#country").html(data);
+                    // $("#country").html(data + "  ");
+                    // $("#country1").html(data[0]);
+                    // $("#country2").html(data[1]);
+                    // $("#country3").html(data[2]);
+                    // $("#country4").html(data[3]);
+                    // $("#country5").html(data[4]);
+
+                    for (i = 0; i <= data.length -1; i ++){
+
+                        $("#country").data("id")
+
+                    }
+                    
 
                 }
             });
